5f563a63942315a63e4931b5e397723520e47911
carloshoyos98 <choyos@cifpfbmoll.eu>
2020-02-20 17:38:55 +0100

    build(CreditCard): Crear test y clase CreditCard.
    Creado el test para el constructor de CreditCard

---
+++ src/main/java/ricksy/business/CreditCard.java
@@ -0,0 +1,13 @@
+package ricksy.business;
+
+public class CreditCard {
+    String owner = "";
+    String number = "";
+    double credit =  3000.0d;
+
+    public CreditCard(String owner, String number) {
+        this.owner = owner;
+        this.number = number;
+    }
+
+}
---
+++ src/test/java/ricksy/business/CreditCardTest.java
@@ -0,0 +1,18 @@
+package ricksy.business;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreditCardTest {
+    public CreditCard tarjetaRick;
+    @Test
+    public void crearTarjetaTest() {
+        //http://ensaimeitor.apsl.net/gen_mastercard/10/
+        tarjetaRick = new CreditCard("Rick", "5238353317461180");
+        assertEquals("Rick", tarjetaRick.owner);
+        assertEquals("5238353317461180", tarjetaRick.number);
+        assertEquals(3000.0, tarjetaRick.credit, 0d);
+
+    }
+}
